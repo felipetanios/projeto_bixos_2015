75341da5648e5fd6bbef823fc29806546d3df003
felipetanios <felipe.tanios@gmail.com>
2014-10-18 11:15:09 -0300

    sem erro, sucesso

--- Orestes/Assets/Scripts/TableMove.cs
+++ Orestes/Assets/Scripts/TableMove.cs
@@ -1,29 +1,38 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class TableMove : MonoBehaviour {
-
+public class TableMove : MonoBehaviour
+{
+	
 	public GameObject Mesa;
 	public Vector2 lugar;
 	private GameObject cloneMesa;
 	// Use this for initialization
-	void Start (){
-		InvokeRepeating ("CriaMesa", 2, 2);
+	void Start()
+	{
+		InvokeRepeating("CriaMesa", 2, 2);
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		foreach (Transform child in transform)
-			child.Translate (lugar * (0.01f));
+	void Update()
+	{
+		foreach (Transform child in transform){
+			if(child){
+				child.Translate(lugar * (0.01f));
+				child.transform.localScale += (0.1F, 0.1F, 0f);
+			}
+		}
 	}
-
-	void CriaMesa(){
-		cloneMesa = Instantiate (Mesa, transform.position, transform.rotation) as GameObject;
+	
+	void CriaMesa()
+	{
+		cloneMesa = Instantiate(Mesa, transform.position, transform.rotation) as GameObject;
 		
-		lugar = new Vector2 (transform.position.x, transform.position.y *(-1));
+		lugar = new Vector2(transform.position.x, transform.position.y * (-1));
 
-		cloneMesa.transform.parent = this.transform;
+		if(cloneMesa)
+			cloneMesa.transform.parent = this.transform;
 		
-		Destroy (cloneMesa, 4f);
+		Destroy(cloneMesa, 4f);
 	}
 }
