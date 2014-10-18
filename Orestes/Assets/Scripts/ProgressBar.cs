c5b8acbe58e16c0cc5cbbd3390de5b07338491eb
Gabriel Souza Franco <gabrielfrancosouza@gmail.com>
2014-10-18 15:14:44 -0300

    Removido o namespace do ProgressBar

--- Orestes/Assets/Scripts/ProgressBar.cs
+++ Orestes/Assets/Scripts/ProgressBar.cs
@@ -1,107 +1,104 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-namespace ProgressBar
+public class ProgressBar : MonoBehaviour
 {
-    public class ProgressBar : MonoBehaviour
-    {
-        private GameObject indicator;
+	private GameObject indicator;
 
-        public delegate void OnComplete();
+	public delegate void OnComplete();
 
-        public OnComplete Complete {
-            get; set;
-        }
+	public OnComplete Complete {
+	    get; set;
+	}
 
-        public float progresso {
-            get;
-            private set;
-        }
+	public float progresso {
+	    get;
+	    private set;
+	}
 
-        private readonly int TEMPO_TOTAL = 2 * 60; //segundos
+	private readonly int TEMPO_TOTAL = 2 * 60; //segundos
 
-        private int flinchCounter = 0;
-        private int blinkCounter = 0;
+	private int flinchCounter = 0;
+	private int blinkCounter = 0;
 
-        private float start;
-        private float width;
+	private float start;
+	private float width;
 
-        // Use this for initialization
-        void Start()
-        {
-            indicator = transform.GetChild(0).gameObject;
+	// Use this for initialization
+	void Start()
+	{
+	    indicator = transform.GetChild(0).gameObject;
 
-            useGUILayout = true;
+	    useGUILayout = true;
 
-            transform.localScale = Vector3.zero;
+	    transform.localScale = Vector3.zero;
 
-            guiTexture.pixelInset = new Rect {
-                x = Screen.width * (1 / 20f),
-                y = Screen.height * (1 / 20f),
-                width = Screen.width * (18 / 20f),
-                height = Screen.width * (1 / 40f)
-            };
-            guiTexture.border = guiTexture.border; //TODO: definir a borda quando tiver a arte final
+	    guiTexture.pixelInset = new Rect {
+	        x = Screen.width * (1 / 20f),
+	        y = Screen.height * (1 / 20f),
+	        width = Screen.width * (18 / 20f),
+	        height = Screen.width * (1 / 40f)
+	    };
+	    guiTexture.border = guiTexture.border; //TODO: definir a borda quando tiver a arte final
 
-            indicator.guiTexture.pixelInset = new Rect {
-                x = Screen.width * (1 / 20f),
-                y = Screen.height * (1 / 20f),
-                width = Screen.width * (1 / 20f), //TODO: tamanhos certos
-                height = Screen.width * (1 / 40f)
-            };
+	    indicator.guiTexture.pixelInset = new Rect {
+	        x = Screen.width * (1 / 20f),
+	        y = Screen.height * (1 / 20f),
+	        width = Screen.width * (1 / 20f), //TODO: tamanhos certos
+	        height = Screen.width * (1 / 40f)
+	    };
 
-            start = guiTexture.pixelInset.x;
-            width = guiTexture.pixelInset.width - indicator.guiTexture.pixelInset.width;
-        }
+	    start = guiTexture.pixelInset.x;
+	    width = guiTexture.pixelInset.width - indicator.guiTexture.pixelInset.width;
+	}
 
-        public void Hit(int power)
-        {
-            if (flinchCounter == 0 && blinkCounter == 0) {
-                flinchCounter = power * 100;
-                blinkCounter = power * 80;
-            } else {
-                // Já foi atingido, aumentar menos o tempo
-                flinchCounter += power * 50;
-                blinkCounter += power * 40;
-            }
-        }
+	public void Hit(int power)
+	{
+	    if (flinchCounter == 0 && blinkCounter == 0) {
+	        flinchCounter = power * 100;
+	        blinkCounter = power * 80;
+	    } else {
+	        // Já foi atingido, aumentar menos o tempo
+	        flinchCounter += power * 50;
+	        blinkCounter += power * 40;
+	    }
+	}
 
-        // FixedUpdate is called once per physics frame
-        void FixedUpdate()
-        {
-            var inset = indicator.guiTexture.pixelInset;
+	// FixedUpdate is called once per physics frame
+	void FixedUpdate()
+	{
+	    var inset = indicator.guiTexture.pixelInset;
 
-            // Dano, retroceder
-            if (flinchCounter > 0) {
+	    // Dano, retroceder
+	    if (flinchCounter > 0) {
 
-                progresso -= 2 * (Time.fixedDeltaTime / TEMPO_TOTAL);
+	        progresso -= 2 * (Time.fixedDeltaTime / TEMPO_TOTAL);
 
-                flinchCounter--;
-                if (progresso == 0)
-                    flinchCounter = 0;
-            } else {
-                progresso += (Time.fixedDeltaTime / TEMPO_TOTAL);
-            }
+	        flinchCounter--;
+	        if (progresso == 0)
+	            flinchCounter = 0;
+	    } else {
+	        progresso += (Time.fixedDeltaTime / TEMPO_TOTAL);
+	    }
 
-            if (blinkCounter > 0) {
-                if (blinkCounter % 24 < 12)
-                    indicator.guiTexture.color = Color.white;
-                else
-                    indicator.guiTexture.color = Color.red;
-                blinkCounter--;
-            }
+	    if (blinkCounter > 0) {
+	        if (blinkCounter % 24 < 12)
+	            indicator.guiTexture.color = Color.white;
+	        else
+	            indicator.guiTexture.color = Color.red;
+	        blinkCounter--;
+	    }
 
-            progresso = Mathf.Clamp01(progresso);
-            inset.x = start + (width * progresso);
+	    progresso = Mathf.Clamp01(progresso);
+	    inset.x = start + (width * progresso);
 
-            indicator.guiTexture.pixelInset = inset;
+	    indicator.guiTexture.pixelInset = inset;
 
-            if (progresso == 1) {
-                // Fim da execução, parar os Updates
-                enabled = false;
-                if (Complete != null)
-                    Complete();
-            }
-        }
-    }
+	    if (progresso == 1) {
+	        // Fim da execução, parar os Updates
+	        enabled = false;
+	        if (Complete != null)
+	            Complete();
+	    }
+	}
 }
