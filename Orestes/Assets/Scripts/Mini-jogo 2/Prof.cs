a69a4093a09c0cc6f793bef5927ea364e3409fa8
felipetanios <felipe.tanios@gmail.com>
2014-11-15 18:49:16 -0200

    chega por hoje

--- Orestes/Assets/Scripts/Mini-jogo 2/Prof.cs
+++ Orestes/Assets/Scripts/Mini-jogo 2/Prof.cs
@@ -8,7 +8,9 @@ public class Prof : MonoBehaviour {
 	private bool isAppearing;
 	private bool isActivated;
 	public Animator moving;
-
+	private GameObject targetSpot;
+	private Transform target;
+	private Vector3 targetPosition;
 
 	private IEnumerator appears;
 	
@@ -18,9 +20,15 @@ public class Prof : MonoBehaviour {
 		isAppearing = true;
 		isActivated = true;
 		gameManagerObject = GameObject.FindGameObjectWithTag("GameManager");
+		targetSpot = GameObject.FindGameObjectWithTag ("AlmostFinal");
 
 		if (gameManagerObject)
 			gameManager = gameManagerObject.GetComponent<GameManager> ();
+
+		if (targetSpot)
+			target = targetSpot.GetComponent<Transform> ();
+
+		targetPosition = target.transform.position;
 
 		totalProfs = gameManager.activatedProfs;
 
@@ -28,27 +36,36 @@ public class Prof : MonoBehaviour {
 	}
 
 	void OnMouseDown() {
-		Destroy (gameObject);
-		
+		Debug.Log ("foi clicado");
 		if (appears != null)
 			StopCoroutine (appears);
 		
 		isActivated = false;
 		gameManager.activatedProfs--;
-		
+
 		gameObject.renderer.enabled = true;
 		gameObject.collider2D.enabled = true;
 		moving.SetBool ("isAnimating", false);
+		transform.position = new Vector3 (transform.position.x, transform.position.y, 2.0F);
+		moving.SetBool ("isClicked", true);
+		FuckYou ();
+
 	}
 
 
 	void Update () {
+		Debug.Log (isActivated);
 		if (!isAppearing && isActivated)
 		{
 			appears = Appears();
             StartCoroutine(appears);
 		}
+		else if (!isActivated && (targetPosition.x - transform.parent.transform.position.x < 0.1)) {
+			FuckYou();
+		}
 	}
+
+
 
 	IEnumerator InitialDelay () 
 	{
@@ -56,7 +73,10 @@ public class Prof : MonoBehaviour {
 		isAppearing = false;
 	}
 
-	IEnumerator Appears () {
+
+
+	IEnumerator Appears () 
+	{
 		isAppearing = true;
 		gameObject.renderer.enabled = true;
 		gameObject.collider2D.enabled = true;
@@ -87,4 +107,20 @@ public class Prof : MonoBehaviour {
 		yield return new WaitForSeconds((gameManager.activatedProfs / totalProfs) + Random.Range (0.5F, 1));
 		isAppearing = false;
 	}
+
+
+
+	void FuckYou ()
+	{
+		Debug.Log (target.position.x + " " + target.position.y);
+		Vector3 velocidade = Vector3.zero;
+		transform.parent.transform.position = Vector3.SmoothDamp (transform.parent.transform.position, targetPosition, ref velocidade, 0.8f);
+		transform.parent.transform.position = new Vector3(transform.parent.transform.position.x, transform.parent.transform.position.y, -5.0f);
+
+	//	float targetPositionX = Mathf.SmoothDamp (transform.parent.transform.position.x , target.position.x, ref velocidade, 0.3F, Time.deltaTime);
+	//	float targetPositionY = Mathf.SmoothDamp (transform.parent.transform.position.y, targetPosition.y , ref velocidade, 0.3F, Time.deltaTime);
+	//	transform.position = new Vector3(targetPositionX, targetPositionY, -3.0f);
+	}
 }
+
+
