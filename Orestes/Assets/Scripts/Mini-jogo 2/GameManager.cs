83d27415a59978e25680341cf770320be6e2dc89
isadorasophia <isaecia@gmail.com>
2014-11-15 01:54:17 -0200

    Início dos scripts responsáveis por gerenciar o aparecimento dos professores no cenário

---
+++ Orestes/Assets/Scripts/Mini-jogo 2/GameManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	[HideInInspector] public int activatedProfs;
+
+	// Use this for initialization
+	void Awake () {
+		activatedProfs = GameObject.FindGameObjectsWithTag ("Prof").Length;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public Vector3 FindSpot () {
+		return new Vector3(0, 0, 0);
+	}
+}
---
+++ Orestes/Assets/Scripts/Mini-jogo 2/Prof.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Prof : MonoBehaviour {
+	private GameObject gameManagerObject;
+	private GameManager gameManager;
+
+	private bool isAppearing = false;
+	private bool isActivated = true;
+
+	private int totalProfs;
+	private 
+
+	void Start () {
+		gameManagerObject = GameObject.FindGameObjectWithTag("GameManager");
+
+		if (gameManagerObject)
+			gameManager = gameManagerObject.GetComponent<GameManager> ();
+
+		totalProfs = gameManager.activatedProfs;
+	}
+
+	void Update () {
+		if (isAppearing)
+		{
+
+		}
+	}
+
+	void OnMouseDown() {
+		isActivated = false;
+		gameManager.activatedProfs--;
+	}
+
+	IEnumerator Appears () {
+		transform.position = gameManager.FindSpot ();
+
+		yield return new WaitForSeconds(gameManager.activatedProfs/totalProfs);
+
+	}
+}
