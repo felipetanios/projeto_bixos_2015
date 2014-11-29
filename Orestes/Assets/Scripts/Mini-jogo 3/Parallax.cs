1fd24bb7f23491b2ee2a344dba2ee95948f695fd
felipetanios <felipe.tanios@gmail.com>
2014-11-29 07:34:17 -0200

    cena com movimento de fundo (pseudo paralaxe com ou sem hifen)

--- Orestes/Assets/Scripts/Mini-jogo 3/Parallax.cs
+++ Orestes/Assets/Scripts/Mini-jogo 3/Parallax.cs
@@ -3,13 +3,31 @@ using System.Collections;
 
 public class Parallax : MonoBehaviour {
 
+	public float spaceGoneInX;
+	public float speedInX;
+	private bool isOnCollider;
 	// Use this for initialization
 	void Start () {
-	
+		isOnCollider = false;
+		spaceGoneInX = 20.0F;
+		speedInX = 0.09F;
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void Update () 
+	{
+		if (isOnCollider) {
+			transform.position = new Vector3(transform.position.x + spaceGoneInX, transform.position.y, transform.position.z);
+			isOnCollider = false;
+		} 
+		else {
+			transform.Translate (-speedInX, 0, 0);
+		}
+	}
+
+
+	void OnTriggerEnter2D(Collider2D other) 
+	{
+		isOnCollider = true;
 	}
 }
