257dc2da4c02cbcc3918e49689b14d4896945e11
Gabriel Souza Franco <gabrielfrancosouza@gmail.com>
2015-02-24 22:47:48 -0300

    Implementação inicial das cutscenes

--- Orestes/Assets/Scripts/StoryTelling/TextBox.cs
+++ Orestes/Assets/Scripts/StoryTelling/TextBox.cs
@@ -1,112 +1,67 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿using System.Collections;
+using System.Text;
+using UnityEngine;
+using UnityEngine.UI;
 
+[RequireComponent(typeof(Text))]
 public class TextBox : MonoBehaviour
 {
-    static string[] texto = {
-        "Texto1", "Texto2", "Texto3", "Texto4", "Texto5", 
-        "Texto6", "Texto7", "Texto8", "Texto9", "Texto10"
-    };
-    int numDeTextos = 10;
+    public string text;
     public float delay = 0.1f;
-	
-    static int size = 25;
-	
-    int x = 5;
-    int y = Screen.height - 3 * size;
-    int w = Screen.width - 3 * size;
-    int h = 3 * size;
-	
-    public int textNumber = 0;
-	
-    bool flag = true;
-    string output = "";
-	
-    bool paused = false;
 
-    void pause()
+    Text textComponent;
+    IEnumerator coroutine;
+
+    void Start()
     {
-        paused = !paused;
-        Time.timeScale = paused ? 0 : 1;
+        textComponent = GetComponent<Text>();
+        coroutine = TypeText();
+        StartCoroutine(coroutine);
     }
 
-    void OnGUI()
+    void Destroy()
     {
-        //GUIStyle myStyle = new GUIStyle(GUI.skin.textField);
-        //myStyle.alignment = TextAnchor.UpperLeft;
-		
-        GUI.color = Color.yellow;
-		
-        //muda as configuracoes da GUI
-        GUI.skin.box.fontSize = size;
-        GUI.skin.box.normal.textColor = Color.yellow;
-        GUI.skin.box.alignment = TextAnchor.UpperLeft;
-        //cria a caixa de texto
-        GUI.Box(new Rect(x, y, w, h), output);
-		
-        //pausa/despausa o texto caso clique 
-        if (!GUI.Button(new Rect(x + w + 5, y, 2 * size + 10, h), "Click")) {
-            pause();
-        }
-		
+        StopCoroutine(coroutine);
     }
 
     IEnumerator TypeText()
     {
-        char letra;
-        int cont = 0;
-        int i;
-		
-        for (i = 0; textNumber < numDeTextos; i++) {
-            //acabou o texto atual
-			
-            if (i + 1 == texto[textNumber].Length)
-                pause();
-            else if (i == texto[textNumber].Length) {
-                textNumber++;
+        StringBuilder sb = new StringBuilder();
 
-                output = "";
-                flag = true;
-                i = -1;
-                cont = 0;
-				
-                continue;
+        var lines = text.Split('\n');
+
+        for (int i = 0; i < lines.Length; i++) {
+            if (i != 0) {
+                sb.Append(lines[i - 1]);
+                sb.Append('\n');
             }
-			
-            letra = texto[textNumber][i];
-			
-            //quebra de linha
-            if ((cont) * (size + 1) >= 2 * w && flag) {
-                output += '\n';
-                cont = 0;
-                flag = false;
+
+            foreach (char c in lines[i]) {
+                sb.Append(c);
+                textComponent.text = sb.ToString();
+                // TODO: sound
+
+                if (!Input.GetButton("Jump"))
+                    yield return new WaitForSeconds(delay);
+                else
+                    yield return new WaitForSeconds(delay / 4);
             }
-			//recomeca a linha
-			else if ((cont) * (size + 1) >= 2 * w && !flag) {
-                output = "";
-                cont = 0;
-                flag = true;
+
+            // First line shouldn't stop
+            if (i != 0) {
+                // Wait for confirmation
+                yield return new WaitForSeconds(.35f);
+                // TODO: arrow
+                while (!Input.GetButton("Jump"))
+                    yield return new WaitForFixedUpdate();
             }
-			//pausa antes de limpar
-			else if ((cont + 1) * (size + 1) >= 2 * w && !flag && !paused) {
-                pause();
-            }
-			
-            output += letra;
-            cont++;
-			
-            //delay da impressao do texto
-            yield return new WaitForSeconds(3 * delay);
+
+            // Clear text
+            // sb.Clear(); ← .NET 4
+            sb.Length = 0;
         }
-    }
 
-    void Start()
-    {
-        StartCoroutine(TypeText());
-    }
-
-    void Update()
-    {
-		
+        // TODO: fire event
+        textComponent.text = string.Empty;
     }
 }
