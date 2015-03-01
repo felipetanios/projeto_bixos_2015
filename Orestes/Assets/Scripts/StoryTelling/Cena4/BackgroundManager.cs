d9962b50cdb97ce9883af207afc5862646a50d71
Gabriel Souza Franco <gabrielfrancosouza@gmail.com>
2015-03-01 12:58:49 -0300

    Cena 4

---
+++ Orestes/Assets/Scripts/StoryTelling/Cena4/BackgroundManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BackgroundManager : ImageManager
+{
+
+    public static new BackgroundManager Instance { get; private set; }
+
+    void Awake()
+    {
+        if (Instance == null)
+            Instance = this;
+        image = GetComponent<Image>();
+    }
+
+    void Destroy()
+    {
+        if (Instance == this)
+            Instance = null;
+    }
+
+    public IEnumerator PanTo(float dest, float duration)
+    {
+        var time = 0f;
+        var position = image.rectTransform.anchoredPosition;
+        var initialPos = position.x;
+        var newPos = initialPos;
+
+        while (!Mathf.Approximately(newPos, dest)) {
+            newPos = Mathf.Lerp(initialPos, dest, time / duration);
+            position.x = newPos;
+            image.rectTransform.anchoredPosition = position;
+
+            time += Time.deltaTime;
+
+            yield return new WaitForFixedUpdate();
+        }
+    }
+}
---
+++ Orestes/Assets/Scripts/StoryTelling/Cena4/Cena4Manager.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(AudioSource))]
+public class Cena4Manager : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        StartCoroutine(Scene());
+    }
+
+    private IEnumerator Scene()
+    {
+        // Delay
+        yield return new WaitForEndOfFrame();
+
+        IEnumerator ret;
+
+        PanelManager.Instance.EnableUI();
+
+        ret = TextBox.Instance.Type(@"(Segundo meu mapa, o IC fica por aqui,
+só preciso subir essa pequena ladeira...)", Color.blue);
+        while (ret.MoveNext())
+            yield return ret.Current;
+
+        ret = TextBox.Instance.Type(@"Não pode ser tão difícil!", false, false);
+        // Need to pan alongside text, so start in another coroutine
+        // PanTo will end after this one, so we can do this
+        StartCoroutine(ret);
+
+        ret = BackgroundManager.Instance.PanTo(-2080, 10f);
+        while (ret.MoveNext())
+            yield return ret.Current;
+
+        yield return new WaitForSeconds(1f);
+
+        ret = BackgroundManager.Instance.FadeTo(0);
+        while (ret.MoveNext())
+            yield return ret.Current;
+
+        Application.LoadLevel("game3");
+    }
+}
