54e05d4ed2636d1a6a7298b308b4bdf93cf6d485
Gabriel Souza Franco <gabrielfrancosouza@gmail.com>
2015-03-02 01:28:27 -0300

    Criar menu inicial

---
+++ Orestes/Assets/Scripts/Menu.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Menu : MonoBehaviour
+{
+
+    public GameObject creditosPanel;
+
+    public void Iniciar()
+    {
+        Application.LoadLevel("cena1");
+    }
+
+    public void MostraCreditos(bool mostra)
+    {
+        creditosPanel.SetActive(mostra);
+    }
+
+}
