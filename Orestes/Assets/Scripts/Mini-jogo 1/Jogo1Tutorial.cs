f0cffd070bfeae9b34c934548300bb01e599f4a2
Gabriel Souza Franco <gabrielfrancosouza@gmail.com>
2015-03-02 14:24:15 -0300

    Tutorial do jogo 1

---
+++ Orestes/Assets/Scripts/Mini-jogo 1/Jogo1Tutorial.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Canvas))]
+public class Jogo1Tutorial : MonoBehaviour
+{
+    public GameObject tutorialPanel;
+
+    // Use this for initialization
+    void Start()
+    {
+        Time.timeScale = 0;
+        tutorialPanel.SetActive(true);
+    }
+
+    public void Juego()
+    {
+        Time.timeScale = 1;
+
+        tutorialPanel.SetActive(false);
+    }
+}
